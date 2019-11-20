14d5ebe1f94ced78f54aba62f09f972a5468a3a4
Brennan McDonald <Brennan.mcdonald.mail@gmail.com>
2019-11-20 17:00:55 -0500

    Fixed hostname issue

--- src/View.js
+++ src/View.js
@@ -9,14 +9,16 @@ export default class View extends React.Component {
             content: "",
             pageNumber: 1,
             numPages: 0,
-            document: {_source: {
-                file: {
-                    url:""
+            document: {
+                _source: {
+                    file: {
+                        url: ""
+                    }
                 }
-            }}
+            }
         }
-        if (!Array.prototype.last){
-            Array.prototype.last = function(){
+        if (!Array.prototype.last) {
+            Array.prototype.last = function() {
                 return this[this.length - 1];
             };
         };
@@ -29,7 +31,7 @@ export default class View extends React.Component {
                     loading: false,
                     document: x,
                     content: "Test" + x._source.content,
-                    file_url: "http://localhost:3000/notes/" + x._source.file.url.replace("file:///home/brennan/Takeout/Drive/School/", "")
+                    file_url: `http://${window.location.hostname}:${window.location.port}/notes/${x._source.file.url.replace("file:///home/brennan/Takeout/Drive/School/", "")}`
                 })
             });
     }
@@ -43,34 +45,38 @@ export default class View extends React.Component {
     decPage = () => {
         const { pageNumber } = this.state;
         if (pageNumber > 1) {
-            this.setState({pageNumber: (pageNumber - 1)})
+            this.setState({ pageNumber: (pageNumber - 1) })
         }
     }
 
     incPage = () => {
         const { pageNumber, numPages } = this.state;
         if (pageNumber < numPages) {
-            this.setState({pageNumber: (pageNumber + 1)})
+            this.setState({ pageNumber: (pageNumber + 1) })
         }
 
     }
 
     render() {
         const { pageNumber, numPages } = this.state;
-        return (
-            <div id="Viewer">
-                    <button onClick={this.decPage}>{"<"}</button>
-                    <div>
-                    <p>{this.state.document._source.file.url.match("[A-Z]{3}[0-9]{4}")} {this.state.document._source.file.url.split("/").last()}</p>
-                <Document
-                    file={this.state.file_url}
-                    onLoadSuccess={this.onDocumentLoadSuccess}>
-                    <Page pageNumber={pageNumber} />
-                </Document>
-                    <p>Page {pageNumber} of {numPages}</p>
-                    </div>
-                    <button onClick={this.incPage}>></button>
-            </div>
+        return ( <
+            div id = "Viewer" >
+            <
+            button onClick = { this.decPage } > { "<" } < /button> <
+            div >
+            <
+            p > { this.state.document._source.file.url.match("[A-Z]{3}[0-9]{4}") } { this.state.document._source.file.url.split("/").last() } < /p> <
+            Document file = { this.state.file_url }
+            onLoadSuccess = { this.onDocumentLoadSuccess } >
+            <
+            Page pageNumber = { pageNumber }
+            /> < /
+            Document > <
+            p > Page { pageNumber }
+            of { numPages } < /p> < /
+            div > <
+            button onClick = { this.incPage } >> < /button> < /
+            div >
         )
     }
 }
