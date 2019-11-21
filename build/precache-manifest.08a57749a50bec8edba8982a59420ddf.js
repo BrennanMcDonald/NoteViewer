ca933e607a585c02f659d8c0dfce671e5087879c
Brennan McDonald <Brennan.mcdonald.mail@gmail.com>
2019-11-20 21:19:42 -0500

    new build

---
+++ build/precache-manifest.08a57749a50bec8edba8982a59420ddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b6c5e36638b6196da08428e7df6135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7120e76725cab74989d6",
+    "url": "/static/css/main.c0a9974b.chunk.css"
+  },
+  {
+    "revision": "9d0a975da5d01f78c0ff",
+    "url": "/static/js/2.0b59a66b.chunk.js"
+  },
+  {
+    "revision": "7120e76725cab74989d6",
+    "url": "/static/js/main.ce9058a7.chunk.js"
+  },
+  {
+    "revision": "de22afbd90cd8e058116",
+    "url": "/static/js/runtime-main.515e228a.js"
+  }
+]);
